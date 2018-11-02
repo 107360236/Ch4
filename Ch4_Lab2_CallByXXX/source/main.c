29dfc75b0022d614b7324a470691ac01bfd5ac2f
107360236 <a0982784312@gmail.com>
2018-11-02 17:00:05 +0800

    Ch4

---
+++ Ch4_Lab2_CallByXXX/source/main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void main(void)
+{
+	int x = 100;
+	int y = addbyone(x);
+	printf("x=%d\n", x);
+	system("pause");
+}
+int addbyone(int x)
+{
+	x++;
+	printf("x=%d\n", x);
+	return x;
+}
